4bd3837fa4bdd010ee8b038a989eeb0b8f8dac3b
Nicholas Robison <nick@nickrobison.com>
2013-10-23 19:53:05 -0400

    Initial Commit

---
+++ DAMNSam3.0/slide_config.js
@@ -0,0 +1,40 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Seeing What Isn\'t There',
+    subtitle: 'The History of Imagining Irreality',
+    //eventInfo: {
+    //  title: 'The DAMN Sam Charters Show<br>3.0',
+    //  date: '9/7/2013'
+    //},
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/damn_tiny.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Nick Robison',
+    //company: 'Job Title<br>Google',
+    //gplus: 'http://plus.google.com/1234567890',
+    twitter: '@nickrobison',
+    www: 'http://www.nickrobison.com',
+    github: 'http://github.com/nickrobison'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
---
+++ README.md
@@ -0,0 +1,24 @@
+Presentations
+=====
+
+A collection of presentations from various talks, classes, and sermons
+that I've accumulated.
+
+Some of the talks include the used images, audio files, or videos. Some
+of those have external copyrights, which should be noted in the
+accompanying presentations.
+
+List of Talks
+====
+
+BCSApologetics [10/23/2013] - 'Apologetics with Nick' | A short talk given at
+Blackhawk Christian High School in their Senior level
+Apologetics class.
+
+DamnSam3.0 [9/13/2013] - 'Seeing what isn't there' | An HTML5
+presentation, given at the 3rd Dman Sam Charters Show on historical attempts to visual invisible processes, and to understand the underlying order of nature.
+
+HSCI101 [10/17/2013] - 'Health and Informatics | Introduction to Health
+Informatics given to Purdue University's HSCI 101 class. Covers the
+major branches of Biomedical and Health Informatics, some examples of
+current research, and potential career paths for aspiring students.
